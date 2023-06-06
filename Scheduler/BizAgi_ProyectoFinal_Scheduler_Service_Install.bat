471fed46b3ec715684dbbe805fb85df468b993f0
anthonellarua <anthonella.rua@tecsup.edu.pe>
2023-06-06 13:24:43 -0500

    first commit

---
+++ Scheduler/BizAgi_ProyectoFinal_Scheduler_Service_Install.bat
@@ -0,0 +1,3 @@
+"C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\installutil.exe" /name=BizAgiProyectoFinalSchedulerService BizAgi.Scheduler.Services.exe
+--NET START "BizAgiProyectoFinalSchedulerService"
+rem pause
---
+++ Scheduler/BizAgi_ProyectoFinal_Scheduler_Service_UnInstall.bat
@@ -0,0 +1,3 @@
+"C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\installutil.exe" /u /name=BizAgiProyectoFinalSchedulerService BizAgi.Scheduler.Services.exe
+sc delete BizAgiProyectoFinalSchedulerService
+rem pause
